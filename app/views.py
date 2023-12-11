93bf91a719dec5256e2e7e00e708817e7d8fd777
udaykumarnasana <udaykumarnasana91@gmail.com>
2023-12-11 16:19:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pavan(request):
+     return render(request,'pavan.html')
+
+def gopu(request):
+     return render(request,'gopu.html')
